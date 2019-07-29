35444845ff0efc73a1dab9b30ca148a3cd66eb4e
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-29 23:55:10 +0300

    Updates

---
+++ precache-manifest.00af2286065ca22be2f5573356331a14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f12d22587c713129b53b6a44495882",
+    "url": "/goit-react-hw-04-pet-store/index.html"
+  },
+  {
+    "revision": "c1f755723e25074cbee7",
+    "url": "/goit-react-hw-04-pet-store/static/css/main.80a6e468.chunk.css"
+  },
+  {
+    "revision": "df2f9b2e46586fb96aa4",
+    "url": "/goit-react-hw-04-pet-store/static/js/2.18f164cd.chunk.js"
+  },
+  {
+    "revision": "c1f755723e25074cbee7",
+    "url": "/goit-react-hw-04-pet-store/static/js/main.138d0633.chunk.js"
+  },
+  {
+    "revision": "b0947ce383148671feb4",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.081fabb4.js"
+  }
+]);
